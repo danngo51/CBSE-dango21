f3c8405ee071fccf104a56e49f7b817e70487cfc
Dan Ngo <danngo51@gmail.com>
2024-05-06 23:31:50 +0200

    Enemy now spawning

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -1,17 +1,37 @@
 package dk.sdu.mmmi.cbse.enemysystem;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 public class EnemyPlugin implements IGamePluginService {
+
+    private Entity enemy;
+
+    public EnemyPlugin(){
+    }
+
+
     @Override
     public void start(GameData gameData, World world) {
+        //Adding entities to the world
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+    }
 
+    private Entity createEnemyShip(GameData gameData) {
+
+        Entity enemyShip = new Enemy();
+        enemyShip.setPolygonCoordinates(-5,-5,10,0,-5,5);
+        enemyShip.setX(gameData.getDisplayHeight()/2);
+        enemyShip.setY(gameData.getDisplayWidth()/2);
+        return enemyShip;
     }
 
     @Override
     public void stop(GameData gameData, World world) {
-
+        //Remove entities
+        world.removeEntity(enemy);
     }
 }
