343859fc5f968f8e8de6831ca91ea93bec9775f9
Dan Ngo <danngo51@gmail.com>
2024-05-19 00:41:42 +0200

    random shutting for enemy

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/Enemy.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/Enemy.java
@@ -3,4 +3,22 @@ package dk.sdu.mmmi.cbse.enemysystem;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
 public class Enemy extends Entity {
+    private int shootInterval;
+    private int shootCounter;
+
+    public int getShootInterval() {
+        return shootInterval;
+    }
+
+    public void setShootInterval(int shootInterval) {
+        this.shootInterval = shootInterval;
+    }
+
+    public int getShootCounter() {
+        return shootCounter;
+    }
+
+    public void setShootCounter(int shootCounter) {
+        this.shootCounter = shootCounter;
+    }
 }
--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -4,19 +4,29 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
+
+import java.util.Collection;
 import java.util.Random;
+import java.util.ServiceLoader;
+
+import static java.util.stream.Collectors.toList;
 
 public class EnemyControlSystem implements IEntityProcessingService {
+
     private Random random = new Random();
     private double speed = 1; //speed for the enemy
     private final double ROTATION_CHANGE_DEGREE = 10; // Maximum rotation change in degrees
     private final double MAX_TURN_DEGREE = 120; // Maximum turn in degrees
-
+    // Shooting interval variables
 
 
     @Override
     public void process(GameData gameData, World world) {
-        for (Entity enemy : world.getEntities(Enemy.class)) {
+        for (Entity entity : world.getEntities(Enemy.class)) {
+            // Casting to enemy to acces some attributes
+            Enemy enemy = (Enemy) entity;
+
             // Randomly adjust rotation within a range of [-5, 5] degrees
             double rotationChange = (random.nextDouble() * 2 * ROTATION_CHANGE_DEGREE) - ROTATION_CHANGE_DEGREE;
             enemy.setRotation(enemy.getRotation() + rotationChange);
@@ -30,6 +40,16 @@ public class EnemyControlSystem implements IEntityProcessingService {
 
             // Check and handle boundaries
             checkAndHandleBoundaries(enemy, gameData);
+
+            // Increment shoot counter and shoot if interval is met
+            enemy.setShootCounter(enemy.getShootCounter()+1);
+            if (enemy.getShootCounter() >= enemy.getShootInterval()) {
+                enemy.setShootCounter(0);
+                enemy.setShootInterval(random.nextInt(85) + 25);
+                getBulletSPIs().stream().findFirst().ifPresent(spi -> {
+                    world.addEntity(spi.createBullet(enemy, gameData));
+                });
+            }
         }
     }
 
@@ -64,5 +84,9 @@ public class EnemyControlSystem implements IEntityProcessingService {
             double randomTurnDegree = (random.nextDouble() * 2 * MAX_TURN_DEGREE) - MAX_TURN_DEGREE;
             updateRotation(enemy, randomTurnDegree);
         }
+    }
+
+    private Collection<? extends BulletSPI> getBulletSPIs() {
+        return ServiceLoader.load(BulletSPI.class).stream().map(ServiceLoader.Provider::get).collect(toList());
     }
 }
