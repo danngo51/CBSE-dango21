a75b6c7ecd50fd1265b4dd22d4c06281d7ccc4e7
Dan Ngo <danngo51@gmail.com>
2024-05-19 23:39:42 +0200

    asteroids now spawning

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
@@ -1,17 +1,42 @@
 package dk.sdu.mmmi.cbse.asteroid;
 
+import dk.sdu.mmmi.cbse.common.asteroid.Asteroid;
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
+import java.util.Random;
+
 public class AsteroidPlugin implements IGamePluginService {
+
+    public AsteroidPlugin() {
+    }
+
     @Override
     public void start(GameData gameData, World world) {
-
+        for(int i = 0; i < 2; i++) {
+            Entity asteroid = createAsteroid(gameData);
+            world.addEntity(asteroid);
+        }
     }
 
     @Override
     public void stop(GameData gameData, World world) {
+        for (Entity asteroid : world.getEntities(Asteroid.class)) {
+            world.removeEntity(asteroid);
+        }
+    }
 
+    private Entity createAsteroid(GameData gameData) {
+        Entity asteroid = new Asteroid();
+        Random rnd = new Random();
+        int size = rnd.nextInt(10) + 5;
+        asteroid.setPolygonCoordinates(size, -size, -size, -size, -size, size, size, size);
+        asteroid.setX(rnd.nextInt(gameData.getDisplayWidth()));
+        asteroid.setY(rnd.nextInt(gameData.getDisplayHeight()));
+        asteroid.setRadius(size);
+        asteroid.setRotation(rnd.nextInt(50));
+        return asteroid;
     }
 }
