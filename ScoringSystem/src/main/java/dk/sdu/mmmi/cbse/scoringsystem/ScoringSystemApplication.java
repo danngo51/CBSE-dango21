be12acff433901cc5591b98c948ffe027ad92cb9
Dan Ngo <danngo51@gmail.com>
2024-05-21 15:05:33 +0200

    Scoring system setup

---
+++ ScoringSystem/src/main/java/dk/sdu/mmmi/cbse/scoringsystem/ScoringSystemApplication.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse.scoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoringSystemApplication.class, args);
+    }
+
+}
